1ae7162e66243bba67ba48f50c83ca23a2953b59
yordy <juanyordyguillen@gmail.com>
2017-08-08 21:35:27 -0500

    validando los parametros

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
 Route::get('/', 'IndexController@actionIndex');
 Route::get('/persona/editar/{idPersona?}','PersonaController@actioneditar')->where(['idPersona'=>'[0-9]*']);
-Route::get('ejemplo/sumar/{parametro1}/{parametro2}','EjemploController@actionesumar');
+Route::get('ejemplo/sumar/{parametro1}/{parametro2}','EjemploController@actionesumar')->where(['parametro1'=>'[0-9]','parametro2'=>'[0-9]']);
 ?>
