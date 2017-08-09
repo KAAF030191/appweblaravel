08b39df394696e427ba056d7fd66dbae9b259408
smptracing <smp.tracing@gmail.com>
2017-08-08 22:05:51 -0500

    Controlador operarsuma y mostrar suma

--- app/Http/Controllers/EjemploController.php
+++ app/Http/Controllers/EjemploController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
+
 class EjemploController extends Controller
 {
 	public  function actionSumar($valora,$valorb)
@@ -10,6 +12,22 @@ class EjemploController extends Controller
 		
 		$sumar=$valora + $valorb;
 		return view('ejemplo/sumar',['sumar'=>$sumar]);
+	}
+
+	public  function actionOperarSuma()
+	{
+		return view ('ejemplo/operarsuma');
+	}
+
+	public function actionMostrarResultado(Request $request)
+	{
+		$numero1=$request->input('txtnumero1');
+		$numero2=$request->input('txtnumero2');
+
+		$resultado = $numero1 + $numero2;
+
+		return view ('ejemplo/mostrarresultado',['resultado'=>$resultado]);
+
 	}
 
 }
