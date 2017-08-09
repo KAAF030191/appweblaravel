7cc49f90d30196c3f4968197a0a30d646e155adb
adlerchirsinos <adlerchirinos@hotmail.com>
2017-08-08 21:34:40 -0500

    prueba de rutas php

---
+++ app/Http/Controllers/ejemplocontroller.php
@@ -0,0 +1,11 @@
+<?php
+namespace App\Http\Controllers;
+
+class PersonaController extends Controller
+{
+	public function actionEditar($idPersona=null)
+	{
+		return view('persona/editar', ['idPersona' => $idPersona]);
+	}
+}
+?>
--- routes/web.php
+++ routes/web.php
@@ -1,3 +1,9 @@
 <?php
 Route::get('/', 'IndexController@actionIndex');
+
+//PERSONA
+Route::get('/persona/editar/{idPersona?}', 'PersonaController@actionEditar')->where(['idPersona' => '[0-9]*']);
+
+
+
 ?>
