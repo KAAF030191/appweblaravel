5d767dd91e13b0112f6eb621e5614f0c7b9df6d3
smptracing <smp.tracing@gmail.com>
2017-08-08 21:28:48 -0500

    Creando ruta ejemplo/sumar/56/78

--- routes/web.php
+++ routes/web.php
@@ -5,4 +5,8 @@ Route::get('/', 'IndexController@actionIndex');
 //editar
 
 Route::get('/persona/editar/{idPersona?}','PersonaController@actionEditar')->where(['idPersona' => '[0-9]*']);
+
+//sumar
+
+Route::get('/ejemplo/sumar/{valora}/{valorb}','EjemploController@actionSumar');
 ?>
