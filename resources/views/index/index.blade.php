07032d7c762670a1a442e6ce22724d430d0743a0
wilberwilly <wilberwillyqm@gmail.com>
2017-08-12 10:39:28 -0500

    clases fecha (zona Horaria)

--- resources/views/index/index.blade.php
+++ resources/views/index/index.blade.php
@@ -1,1 +1,17 @@
+@extends('template.templategeneric')
+@section('cuerpoGeneral')
 <b>Holaaaa!!!!!</b>
+<br><br>
+<?php
+$nombre='Juan';
+echo 'Hola $nombre';
+echo '<br>';
+echo "Hola $nombre";
+?>
+<br><br>
+{{'Este es $nombre'}}
+<br>
+{{"Este es $nombre"}}
+<hr>
+{{date('Y-m-d H:i:s')}}
+@endsection
