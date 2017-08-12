e3014fee823ea84d82fbfc4b97244c19766bceea
flower919 <florescobar919@gmail.com>
2017-08-12 11:38:39 -0500

    Se agrego bootstrap y jquery..

--- resources/views/persona/insertar.blade.php
+++ resources/views/persona/insertar.blade.php
@@ -3,14 +3,15 @@
 <h1>Insertar Persona</h1>
 <form action={{URL('/persona/insertar')}} method="post">
 	<label>Nombre: </label>
-	<input type="text" name="txtNombre">
+	<input type="text" name="txtNombre" class="form-control">
 	<label>Apellido: </label>
-	<input type="text" name="txtApellido">
+	<input type="text" name="txtApellido" class="form-control">
 	{{csrf_field()}}
-	<input type="submit" name="" value="Guardar">	
+	<input type="submit" name="" value="Guardar" class="btn btn-primary">	
 </form>
 <div style="border: 1px solid;">
 	<p>Nombre: {{$nombre or ''}} </p>
 	<p>Apellido: {{$apellido or ''}} </p>
 </div>
+<p>Fecha: {{date('Y-m-d H:i:s')}} </p>
 @endsection
--- resources/views/template/templategeneric.blade.php
+++ resources/views/template/templategeneric.blade.php
@@ -3,6 +3,9 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Document</title>
+	<link rel="stylesheet" type="text/css" href="{{asset('css/bootstrap.min.css')}}">
+	<script type="text/javascript" src="{{asset('js/jquery-3.2.1.min.js')}}" ></script>
+	<script type="text/javascript" src="{{asset('js/bootstrap.min.js')}}" ></script>
 </head>
 <body>
 	<header>@include('template/partial/headerbanner')</header>
