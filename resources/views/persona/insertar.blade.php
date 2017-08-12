59df7c75852750414e13ffdc987958afe6b14050
wilberwilly <wilberwillyqm@gmail.com>
2017-08-11 22:24:01 -0500

    uuuu

---
+++ resources/views/persona/insertar.blade.php
@@ -0,0 +1,16 @@
+@extends('template.templategeneric')
+@section('cuerpoGeneral')
+<form action="{{url('persona/insertar')}}" method="post">
+	<label for="txtNombre">Nombre</label>
+	<input type="text" id="txtNombre" name="txtNombre">
+	<label for="txtApellido">Apellido</label>
+	<input type="text" id="txtApellido" name="txtApellido">
+	{{csrf_field()}}
+	<input type="submit" value="Registrar datos">
+</form>
+<div style="border: 1px solid #999999;margin: 5px;">
+	<b>Nombre: </b>{{$nombre or ''}}
+	<br>
+	<b>Apellido: </b>{{$apellido or ''}}
+</div>
+@endsectiond
