7d2cdda46d9775cb08c30715b7bab38971f681c0
wilberwilly <wilberwillyqm@gmail.com>
2017-08-12 09:12:35 -0500

    actualizando a clase

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,8 @@
 Route::get('/', 'IndexController@actionIndex');
 
 //PERSONA
-Route::get('/persona/editar/{idPersona?}', 'PersonaController@actionEditar')->where(['idPersona' => '[0-9]*']);
+Route::match(['get', 'post'], '/persona/insertar', 'PersonaController@actionInsertar');
+//Route::get('/persona/editar/{idPersona?}', 'PersonaController@actionEditar')->where(['idPersona' => '[0-9]*']);
 
 //EJEMPLO
 Route::get('/ejemplo/sumar/{numeroUno}/{numeroDos}', 'EjemploController@actionSumar')->where(['numeroUno' => '[0-9]+', 'numeroDos' => '[0-9]+']);
