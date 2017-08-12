5e1c2e3b9a33e4389c64dce6a3521c173f90b56a
flower919 <florescobar919@gmail.com>
2017-08-12 12:13:20 -0500

    -

--- resources/views/persona/insertar.blade.php
+++ resources/views/persona/insertar.blade.php
@@ -2,16 +2,28 @@
 @section('CuerpoGeneral')
 <h1>Insertar Persona</h1>
 <form action={{URL('/persona/insertar')}} method="post">
-	<label>Nombre: </label>
-	<input type="text" name="txtNombre" class="form-control">
-	<label>Apellido: </label>
-	<input type="text" name="txtApellido" class="form-control">
-	{{csrf_field()}}
-	<input type="submit" name="" value="Guardar" class="btn btn-primary">	
+	<div class="row">
+		<div class="col-sm-4">
+			<label>Nombre: </label>
+			<div>
+				<input type="text" name="txtNombre" class="form-control">			
+			</div>		
+		</div>
+		<div class="col-sm-4">
+			<label>Apellido: </label>
+			<div>
+				<input type="text" name="txtApellido" class="form-control">
+			</div>		
+		</div>
+		<div class="col-sm-4">
+			{{csrf_field()}}
+			<input type="submit" name="" value="Guardar" class="btn btn-primary" style="margin-top: 10px;">		
+		</div>
+	</div>
 </form>
-<div style="border: 1px solid;">
+<!--<div style="border: 1px solid;">
 	<p>Nombre: {{$nombre or ''}} </p>
 	<p>Apellido: {{$apellido or ''}} </p>
 </div>
-<p>Fecha: {{date('Y-m-d H:i:s')}} </p>
+<p>Fecha: {{date('Y-m-d H:i:s')}} </p>-->
 @endsection
